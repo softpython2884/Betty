86dc594e93a7edca461dfd60cd592b9e5b8de0ac
Soft Informatique <softinformatique83@gmail.com>
2025-07-18 15:39:33 +0000

    nan, genre diminue l'epesseur de la bar de droite, le truc statique dans

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -227,14 +227,13 @@ export default function ProjectWorkspacePage({ params }: { params: { projectId: 
                     <TabsContent value="codespace" className="mt-6">
                         <Card className="text-center">
                             <CardHeader>
-                                <CardTitle>CodeSpace</CardTitle>
-                                <CardDescription>Connectez votre dépôt GitHub pour commencer à coder.</CardDescription>
+                                <CardTitle>FlowUp CodeSpace</CardTitle>
+                                <CardDescription>Connectez-vous pour commencer à coder dans votre environnement FlowUp.</CardDescription>
                             </CardHeader>
                             <CardContent className="py-12">
-                                <GitMerge className="h-24 w-24 mx-auto text-muted-foreground/30 mb-6"/>
+                                <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-24 w-24 mx-auto text-muted-foreground/30 mb-6 fill-current"><title>FlowUp</title><path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm0 22.286c-5.663 0-10.286-4.623-10.286-10.286S6.337 1.714 12 1.714 22.286 6.337 22.286 12 17.663 22.286 12 22.286zm-1.714-13.714h3.428v10.286h-3.428V8.572zm1.714-5.143a2.571 2.571 0 110-5.142 2.571 2.571 0 010 5.142z"/></svg>
                                 <Button size="lg">
-                                    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="mr-2 h-5 w-5 fill-current"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
-                                    Connecter avec GitHub
+                                    Ouvrir FlowUp CodeSpace
                                 </Button>
                             </CardContent>
                         </Card>
@@ -301,5 +300,3 @@ export default function ProjectWorkspacePage({ params }: { params: { projectId: 
         </AppShell>
     );
 }
-
-    
