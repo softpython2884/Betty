14fb2158d2a67affa1d76dc8190d3c1cca08a7d3
mattéo magnani <matteojulienmagnani@gmail.com>
2025-07-18 21:41:19 +0000

    quand je veut invitée un pers :

--- drizzle/0002_wandering_ego.sql
+++ drizzle/0002_wandering_ego.sql
@@ -1,3 +1,1 @@
-
-ALTER TABLE users ADD `must_change_password` integer DEFAULT false;--> statement-breakpoint
-ALTER TABLE users ADD `status` text DEFAULT 'active' NOT NULL;
+ALTER TABLE `users` ADD `must_change_password` integer DEFAULT false;
