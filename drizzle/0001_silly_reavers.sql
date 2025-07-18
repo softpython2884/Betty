dabb61abbbaf8b847f6e024cc3bdc36c0fdaf638
mattéo magnani <matteojulienmagnani@gmail.com>
2025-07-18 21:12:09 +0000

    Migration failed: Failed to run the query '/* SQLite does not support al

--- drizzle/0001_silly_reavers.sql
+++ drizzle/0001_silly_reavers.sql
@@ -1,9 +1,3 @@
-/*
- SQLite does not support altering a table to add a foreign key constraint or a NOT NULL column with no default value.
- We must therefore recreate the table.
-*/
-
--- Create the new curriculums table
 CREATE TABLE `curriculums` (
 	`id` text PRIMARY KEY NOT NULL,
 	`name` text NOT NULL,
@@ -13,24 +7,7 @@ CREATE TABLE `curriculums` (
 	`created_at` integer NOT NULL,
 	FOREIGN KEY (`created_by`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
 );
-
--- Drop the old quests table
-DROP TABLE `quests`;
-
--- Recreate the quests table with the new curriculum_id foreign key
-CREATE TABLE `quests` (
-	`id` text PRIMARY KEY NOT NULL,
-	`title` text NOT NULL,
-	`description` text,
-	`category` text NOT NULL,
-	`xp` integer NOT NULL,
-    `orbs` integer DEFAULT 0,
-	`status` text NOT NULL,
-	`position_top` text NOT NULL,
-	`position_left` text NOT NULL,
-	`curriculum_id` text NOT NULL,
-	FOREIGN KEY (`curriculum_id`) REFERENCES `curriculums`(`id`) ON UPDATE no action ON DELETE no action
-);
-
--- Note: All previous quest data is lost in this process.
--- You might want to back up and restore data if this were a production environment.
+--> statement-breakpoint
+ALTER TABLE quests ADD `orbs` integer DEFAULT 0;--> statement-breakpoint
+ALTER TABLE quests ADD `curriculum_id` text NOT NULL;--> statement-breakpoint
+ALTER TABLE `quests` DROP COLUMN `curriculum`;
