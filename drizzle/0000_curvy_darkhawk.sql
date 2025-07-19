7d05f5cb932413d23612ab7f0c836e2cf6e99e2b
Soft Informatique <softinformatique83@gmail.com>
2025-07-19 01:57:20 +0000

    bas jsp, SQL directement ? mais genre jsp... ce qui et utiliser dans le

--- drizzle/0000_curvy_darkhawk.sql
+++ drizzle/0000_curvy_darkhawk.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `quiz_options` (
+CREATE TABLE `quizzes` (
 	`id` text PRIMARY KEY NOT NULL,
-	`question_id` text NOT NULL,
-	`text` text NOT NULL,
-	`is_correct` integer DEFAULT false NOT NULL,
-	FOREIGN KEY (`question_id`) REFERENCES `quiz_questions`(`id`) ON UPDATE no action ON DELETE no action
+	`title` text NOT NULL,
+	`quest_id` text,
+	`passing_score` integer DEFAULT 80 NOT NULL,
+	FOREIGN KEY (`quest_id`) REFERENCES `quests`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
 CREATE TABLE `quiz_questions` (
@@ -14,12 +14,17 @@ CREATE TABLE `quiz_questions` (
 	FOREIGN KEY (`quiz_id`) REFERENCES `quizzes`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
-CREATE TABLE `quizzes` (
+CREATE TABLE `quiz_options` (
 	`id` text PRIMARY KEY NOT NULL,
-	`title` text NOT NULL,
-	`quest_id` text,
-	`passing_score` integer DEFAULT 80 NOT NULL,
-	FOREIGN KEY (`quest_id`) REFERENCES `quests`(`id`) ON UPDATE no action ON DELETE no action
+	`question_id` text NOT NULL,
+	`text` text NOT NULL,
+	`is_correct` integer DEFAULT false NOT NULL,
+	FOREIGN KEY (`question_id`) REFERENCES `quiz_questions`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
-CREATE UNIQUE INDEX `quizzes_quest_id_unique` ON `quizzes` (`quest_id`);
+/*
+ SQLite does not support "Creating foreign key on existing column" out of the box, we do not generate automatic migration for that, so it has to be done manually
+ Please refer to: https://www.techonthenet.com/sqlite/foreign_keys/alter_table.php
+
+Due to that we don't generate migration automatically and it has to be done manually
+*/
